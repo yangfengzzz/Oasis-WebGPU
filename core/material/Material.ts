ec5a353de09e4396559421ed9cdc6123826a5225
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-24 15:41:03 +0800

    add shader data

--- core/material/Material.ts
+++ core/material/Material.ts
@@ -1,67 +1,50 @@
-import { CloneManager } from "../clone/CloneManager";
+import {CloneManager} from "../clone/CloneManager";
 import {RenderQueueType} from "./enums/RenderQueueType";
+import {RenderState} from "../shader/state/RenderState";
+import {Shader} from "../shader/Shader";
+import {ShaderData} from "../shader/ShaderData";
+import {ShaderDataGroup} from "../shader/ShaderDataGroup";
 
 /**
  * Material.
  */
 export class Material {
-  /** Name. */
-  name: string;
-  // /** Shader used by the material. */
-  // shader: Shader;
-  /** Render queue type. */
-  renderQueueType: RenderQueueType = RenderQueueType.Opaque;
-  // /** Shader data. */
-  // readonly shaderData: ShaderData = new ShaderData(ShaderDataGroup.Material);
-  // /** Render state. */
-  // readonly renderState: RenderState = new RenderState(); // todo: later will as a part of shaderData when shader effect frame is OK, that is more powerful and flexible.
+    /** Name. */
+    name: string;
+    /** Shader used by the material. */
+    shader: Shader;
+    /** Render queue type. */
+    renderQueueType: RenderQueueType = RenderQueueType.Opaque;
+    /** Shader data. */
+    readonly shaderData: ShaderData = new ShaderData(ShaderDataGroup.Material);
+    /** Render state. */
+    readonly renderState: RenderState = new RenderState(); // todo: later will as a part of shaderData when shader effect frame is OK, that is more powerful and flexible.
 
-  /**
-   * Create a material instance.
-   * @param engine - Engine to which the material belongs
-   * @param shader - Shader used by the material
-   */
-  constructor(engine: Engine, shader: Shader) {
-    super(engine);
-    this.shader = shader;
-  }
+    /**
+     * Create a material instance.
+     * @param shader - Shader used by the material
+     */
+    constructor(shader: Shader) {
+        this.shader = shader;
+    }
 
-  /**
-   * Clone and return the instance.
-   */
-  clone(): Material {
-    const dest = new Material(this._engine, this.shader);
-    this.cloneTo(dest);
-    return dest;
-  }
+    /**
+     * Clone and return the instance.
+     */
+    clone(): Material {
+        const dest = new Material(this.shader);
+        this.cloneTo(dest);
+        return dest;
+    }
 
-  /**
-   * Clone to the target material.
-   * @param target - target material
-   */
-  cloneTo(target: Material): void {
-    target.shader = this.shader;
-    target.renderQueueType = this.renderQueueType;
-    this.shaderData.cloneTo(target.shaderData);
-    CloneManager.deepCloneObject(this.renderState, target.renderState);
-  }
-
-  /**
-   * @override
-   */
-  _addRefCount(value: number): void {
-    super._addRefCount(value);
-    this.shaderData._addRefCount(value);
-  }
-
-  /**
-   * @internal
-   * @todo:temporary solution
-   */
-  _preRender(renderElement: RenderElement) {}
-
-  /**
-   * @override
-   */
-  protected _onDestroy(): void {}
+    /**
+     * Clone to the target material.
+     * @param target - target material
+     */
+    cloneTo(target: Material): void {
+        target.shader = this.shader;
+        target.renderQueueType = this.renderQueueType;
+        this.shaderData.cloneTo(target.shaderData);
+        CloneManager.deepCloneObject(this.renderState, target.renderState);
+    }
 }
