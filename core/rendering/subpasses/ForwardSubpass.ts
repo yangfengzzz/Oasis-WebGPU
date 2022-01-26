ca7c2838bd586b511dba374c345e11f3e90884c4
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-26 08:42:23 +0800

    refactor whole process

--- core/rendering/subpasses/ForwardSubpass.ts
+++ core/rendering/subpasses/ForwardSubpass.ts
@@ -1,5 +1,4 @@
 import {Subpass} from "../Subpass";
-import {RenderContext} from "../RenderContext";
 import {Scene} from "../../Scene";
 import {Camera} from "../../Camera";
 import {RenderPipelineDescriptor} from "../../webgpu/RenderPipelineDescriptor";
@@ -57,32 +56,85 @@ export class ForwardSubpass extends Subpass {
     private _renderPipeline: GPURenderPipeline;
 
     private _shaderProgram: ShaderProgram;
+    private _pBuffer: Buffer;
+    private _mvBuffer: Buffer;
+    private _box: ModelMesh;
 
     constructor(engine: Engine) {
         super(engine);
+        const device = this._engine.device;
+        this._shaderProgram = new ShaderProgram(device, vxCode, fxCode);
+        this._box = PrimitiveMesh.createCuboid(this._engine, 1);
+        {
+            this._bindGroupLayoutDescriptor.entries.length = 2;
+            const uniform1 = new BindGroupLayoutEntry();
+            uniform1.binding = 0;
+            uniform1.visibility = GPUShaderStage.VERTEX;
+            uniform1.buffer = new BufferBindingLayout();
+            uniform1.buffer.type = 'uniform';
+            this._bindGroupLayoutDescriptor.entries[0] = uniform1;
+            const uniform2 = new BindGroupLayoutEntry();
+            uniform2.binding = 1;
+            uniform2.visibility = GPUShaderStage.VERTEX;
+            uniform2.buffer = new BufferBindingLayout();
+            uniform2.buffer.type = 'uniform';
+            this._bindGroupLayoutDescriptor.entries[1] = uniform2;
+            this._uniformGroupLayout = device.createBindGroupLayout(this._bindGroupLayoutDescriptor);
+        }
+        {
+            this._pBuffer = new Buffer(this._engine, 64, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST);
+            this._pBuffer._nativeBuffer.unmap();
+            this._mvBuffer = new Buffer(this._engine, 64, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST);
+            this._mvBuffer._nativeBuffer.unmap();
+            this._bindGroupDescriptor.layout = this._uniformGroupLayout;
+            this._bindGroupDescriptor.entries.length = 2;
+            const uniform1 = new BindGroupEntry();
+            uniform1.binding = 0;
+            const uniformBuffer1 = new BufferBinding();
+            uniformBuffer1.buffer = this._pBuffer._nativeBuffer;
+            uniform1.resource = uniformBuffer1;
+            this._bindGroupDescriptor.entries[0] = uniform1;
+            const uniform2 = new BindGroupEntry();
+            uniform2.binding = 1;
+            const uniformBuffer2 = new BufferBinding();
+            uniformBuffer2.buffer = this._mvBuffer._nativeBuffer;
+            uniform2.resource = uniformBuffer2;
+            this._bindGroupDescriptor.entries[1] = uniform2;
+            this._uniformBindGroup = device.createBindGroup(this._bindGroupDescriptor);
+        }
         this._forwardPipelineDescriptor.depthStencil = this._depthStencilState;
         this._forwardPipelineDescriptor.fragment = this._fragment;
         this._forwardPipelineDescriptor.primitive = this._primitive;
         this._forwardPipelineDescriptor.multisample = this._multisample;
         this._forwardPipelineDescriptor.vertex = this._vertex;
         this._forwardPipelineDescriptor.label = "Forward Pipeline";
+        {
+            this._pipelineLayoutDescriptor.bindGroupLayouts.length = 1;
+            this._pipelineLayoutDescriptor.bindGroupLayouts[0] = this._uniformGroupLayout;
+            this._pipelineLayout = device.createPipelineLayout(this._pipelineLayoutDescriptor);
+            this._forwardPipelineDescriptor.layout = this._pipelineLayout;
 
-        this._bindGroupLayoutDescriptor.entries.length = 2;
-        const uniform1 = new BindGroupLayoutEntry();
-        uniform1.binding = 0;
-        uniform1.visibility = GPUShaderStage.VERTEX;
-        uniform1.buffer = new BufferBindingLayout();
-        uniform1.buffer.type = 'uniform';
-        this._bindGroupLayoutDescriptor.entries[0] = uniform1;
-        const uniform2 = new BindGroupLayoutEntry();
-        uniform2.binding = 1;
-        uniform2.visibility = GPUShaderStage.VERTEX;
-        uniform2.buffer = new BufferBindingLayout();
-        uniform2.buffer.type = 'uniform';
-        this._bindGroupLayoutDescriptor.entries[1] = uniform2;
-        this._uniformGroupLayout = this._engine.device.createBindGroupLayout(this._bindGroupLayoutDescriptor);
+            this._vertex.module = this._shaderProgram.vertexShader;
+            this._vertex.entryPoint = 'main';
 
-        this._shaderProgram = new ShaderProgram(this._engine.device, vxCode, fxCode);
+            this._fragment.module = this._shaderProgram.fragmentShader;
+            this._fragment.entryPoint = 'main';
+            this._fragment.targets.length = 1;
+            const colorTargetState = new ColorTargetState();
+            colorTargetState.format = this._engine.renderContext.drawableTextureFormat();
+            this._fragment.targets[0] = colorTargetState;
+
+            this._primitive.topology = 'triangle-list';
+
+            this._depthStencilState.depthWriteEnabled = true;
+            this._depthStencilState.depthCompare = 'less';
+            this._depthStencilState.format = this._engine.renderContext.depthStencilTextureFormat();
+
+            this._multisample.count = 1;
+
+            this._bindVertexAttrib(this._box);
+            this._renderPipeline = device.createRenderPipeline(this._forwardPipelineDescriptor);
+        }
     }
 
     prepare(): void {
@@ -99,80 +151,22 @@ export class ForwardSubpass extends Subpass {
         animate();
         triangleMVMatrix.identity().translate(new Vector3(-1.5, 0.0, -7.0)).multiply(new Matrix().rotateAxisAngle(new Vector3(0, 1, 0), rTri));
         squareMVMatrix.identity().translate(new Vector3(1.5, 0.0, -7.0)).multiply(new Matrix().rotateAxisAngle(new Vector3(1, 0, 0), rSquare));
+        this._updateUniformBuffer(pMatrix.elements, triangleMVMatrix.elements, renderPassEncoder);
 
-        this._createUniformBuffer(pMatrix.elements, triangleMVMatrix.elements, renderPassEncoder);
-        const box = PrimitiveMesh.createCuboid(this._engine, 1);
-        this._drawElement(renderPassEncoder, box, box.subMesh);
-
-        this._createUniformBuffer(pMatrix.elements, squareMVMatrix.elements, renderPassEncoder);
-        const sphere = PrimitiveMesh.createSphere(this.engine, 1, 50);
-        this._drawElement(renderPassEncoder, sphere, sphere.subMesh);
+        this._drawElement(renderPassEncoder, this._box, this._box.subMesh);
     }
 
     private _drawElement(renderPassEncoder: GPURenderPassEncoder, primitive: ModelMesh, subMesh: SubMesh) {
-        this._createRenderPipeline(primitive);
         renderPassEncoder.setPipeline(this._renderPipeline);
-
         renderPassEncoder.setVertexBuffer(0, primitive._vertexBufferBindings[0]._buffer._nativeBuffer);
         renderPassEncoder.setIndexBuffer(primitive._indexBufferBinding.buffer._nativeBuffer, "uint32");
         renderPassEncoder.drawIndexed(subMesh.count, 1, subMesh.start, 0, 0);
     }
 
-    private _createUniformBuffer(pArray: Float32Array, mvArray: Float32Array, commandEncoder: GPURenderPassEncoder) {
-        let pBuffer = new Buffer(this._engine, pArray, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST);
-        pBuffer.setData(pArray);
-        let mvBuffer = new Buffer(this._engine, mvArray, GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST);
-        mvBuffer.setData(mvArray);
-
-        this._bindGroupDescriptor.layout = this._uniformGroupLayout;
-        this._bindGroupDescriptor.entries.length = 2;
-        const uniform1 = new BindGroupEntry();
-        uniform1.binding = 0;
-        const uniformBuffer1 = new BufferBinding();
-        uniformBuffer1.buffer = pBuffer._nativeBuffer;
-        uniform1.resource = uniformBuffer1;
-        this._bindGroupDescriptor.entries[0] = uniform1;
-        const uniform2 = new BindGroupEntry();
-        uniform2.binding = 1;
-        const uniformBuffer2 = new BufferBinding();
-        uniformBuffer2.buffer = mvBuffer._nativeBuffer;
-        uniform2.resource = uniformBuffer2;
-        this._bindGroupDescriptor.entries[1] = uniform2;
-
-        this._uniformBindGroup = this._engine.device.createBindGroup(this._bindGroupDescriptor);
-
+    private _updateUniformBuffer(pArray: Float32Array, mvArray: Float32Array, commandEncoder: GPURenderPassEncoder) {
+        this._engine.device.queue.writeBuffer(this._pBuffer._nativeBuffer, 0, pArray);
+        this._engine.device.queue.writeBuffer(this._mvBuffer._nativeBuffer, 0, mvArray);
         commandEncoder.setBindGroup(0, this._uniformBindGroup);
-    }
-
-    private _createRenderPipeline(primitive: ModelMesh) {
-        const device = this._engine.device;
-
-        this._pipelineLayoutDescriptor.bindGroupLayouts.length = 1;
-        this._pipelineLayoutDescriptor.bindGroupLayouts[0] = this._uniformGroupLayout;
-        this._pipelineLayout = device.createPipelineLayout(this._pipelineLayoutDescriptor);
-
-        this._forwardPipelineDescriptor.layout = this._pipelineLayout;
-
-        this._vertex.module = this._shaderProgram.vertexShader;
-        this._vertex.entryPoint = 'main';
-
-        this._fragment.module = this._shaderProgram.fragmentShader;
-        this._fragment.entryPoint = 'main';
-        this._fragment.targets.length = 1;
-        const colorTargetState = new ColorTargetState();
-        colorTargetState.format = this._engine.renderContext.drawableTextureFormat();
-        this._fragment.targets[0] = colorTargetState;
-
-        this._primitive.topology = 'triangle-list';
-
-        this._depthStencilState.depthWriteEnabled = true;
-        this._depthStencilState.depthCompare = 'less';
-        this._depthStencilState.format = this._engine.renderContext.depthStencilTextureFormat();
-
-        this._multisample.count = 1;
-
-        this._bindVertexAttrib(primitive);
-        this._renderPipeline = device.createRenderPipeline(this._forwardPipelineDescriptor);
     }
 
     private _bindVertexAttrib(primitive: ModelMesh) {
