37dfa53bd35f82b3c14acd5e8ba2acaf6f49dd36
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-09 16:22:58 +0800

    refactor ShaderProgram

--- core/shader/ShaderProgram.ts
+++ core/shader/ShaderProgram.ts
@@ -1,11 +1,23 @@
+import {ShaderModuleDescriptor} from "../webgpu";
+
 /**
  * Shader program, corresponding to the GPU shader program.
  * @internal
  */
 export class ShaderProgram {
-    vertexShader: GPUShaderModule;
-    fragmentShader: GPUShaderModule;
+    private static _shaderModuleDescriptor: ShaderModuleDescriptor = new ShaderModuleDescriptor();
+
+    private _vertexShader: GPUShaderModule;
+    private _fragmentShader: GPUShaderModule;
     private _device: GPUDevice;
+
+    get vertexShader(): GPUShaderModule {
+        return this._vertexShader;
+    }
+
+    get fragmentShader(): GPUShaderModule {
+        return this._fragmentShader;
+    }
 
     constructor(device: GPUDevice, vertexSource: string, fragmentSource: string) {
         this._device = device;
@@ -16,12 +28,10 @@ export class ShaderProgram {
      * init and link program with shader.
      */
     private _createProgram(vertexSource: string, fragmentSource: string) {
-        this.vertexShader = this._device.createShaderModule({
-            code: vertexSource
-        });
+        ShaderProgram._shaderModuleDescriptor.code = vertexSource;
+        this._vertexShader = this._device.createShaderModule(ShaderProgram._shaderModuleDescriptor);
 
-        this.fragmentShader = this._device.createShaderModule({
-            code: fragmentSource
-        });
+        ShaderProgram._shaderModuleDescriptor.code = fragmentSource;
+        this._fragmentShader = this._device.createShaderModule(ShaderProgram._shaderModuleDescriptor);
     }
 }
