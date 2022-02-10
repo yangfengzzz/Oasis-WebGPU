04943c4d12774afb0ec85517ab7b7057b5601906
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-10 13:55:59 +0800

    add WGSLWorldPosVert

---
+++ core/shaderlib/functors/WGSLPositionVert.ts
@@ -0,0 +1,15 @@
+import {ShaderData} from "../../shader";
+
+export class WGSLPositionVert {
+    private readonly _input: string;
+    private readonly _output: string;
+
+    constructor(input: string, output: string) {
+        this._input = input;
+        this._output = output;
+    }
+
+    execute(macros: ShaderData): string {
+        return `${this._output}.position = u_rendererData.u_MVPMat * position;\n`;
+    }
+}
