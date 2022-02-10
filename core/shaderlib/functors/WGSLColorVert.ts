7d043a8e8b14e1e89305b1347276960664f8db75
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-10 13:49:02 +0800

    add WGSLColorVert

---
+++ core/shaderlib/functors/WGSLColorVert.ts
@@ -0,0 +1,18 @@
+import {ShaderData} from "../../shader";
+
+export class WGSLColorVert {
+    private readonly _input: string;
+    private readonly _output: string;
+
+    constructor(input: string, output: string) {
+        this._input = input;
+        this._output = output;
+    }
+
+    execute(macros: ShaderData): string {
+        if (macros.isEnable("HAS_VERTEXCOLOR")) {
+            return `${this._output}.v_color = ${this._input}.COLOR_0;\n`;
+        }
+        return "";
+    }
+}
